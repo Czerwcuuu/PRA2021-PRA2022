764f182b66be04a1fe8199fd28df6bcfc7a631bd
Czerwcuu <czerwiecr1999@gmail.com>
2022-03-12 09:17:41 +0100

    maven fix

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
+        Logger log = Logger.getLogger("name");
+
+        log.info("message");
         System.out.println("Hello! Witaj!");
     }
 }
